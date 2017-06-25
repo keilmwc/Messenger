b1b4e2c4e3e04617988d0c689acbd3f899461dae
Keil Carpenter <development@keilcarpenter.pro>
2017-06-25 20:22:27 +1200

    Set up User mongoose model

--- models/message.js
+++ models/message.js
@@ -4,9 +4,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// Blueprint for mongo model
 var schema = new Schema({
     content: {type: String, required: true},
-    user: {type: Schema.Types.ObjectId}
+    user: {type: Schema.Types.ObjectId, ref: 'User'}
 });
 
-module.exports = mongoose.model('Message');
+// Build model named Message based on the specified schema (blueprint)
+module.exports = mongoose.model('Message', schema);
--- models/user.js
+++ models/user.js
@@ -1,3 +1,21 @@
 /**
  * Created by KeilCarpenter on 25/06/2017.
  */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongooseUniqueValidator = require('mongoose-unique-validator');
+
+// Blueprint for model
+var schema = new Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    messages: [{type: Schema.Types.ObjectId, ref: 'Message'}]
+});
+
+// Use unique validator
+schema.plugin(mongooseUniqueValidator);
+
+// Build 'User' model based on the specified schema
+module.exports = mongoose.model('User', schema);
