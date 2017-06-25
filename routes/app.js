b7521552b05b0acaab28f9de33b834182b8518e3
Keil Carpenter <development@keilcarpenter.pro>
2017-06-25 20:47:28 +1200

    Message routes and form

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,13 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+router.get('/message/:message', function (req, res, next) {
+    res.render('node', {message: req.params.message});
+});
+
+router.post('/message', function(req, res, next){
+    var message = req.body.message;
+    res.redirect('/message/' + message);
+});
+
 module.exports = router;
