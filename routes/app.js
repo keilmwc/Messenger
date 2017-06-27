a24143879f0235ffc46e6037e01c7f0bfbae043e
Keil Carpenter <development@keilcarpenter.pro>
2017-06-27 22:08:30 +1200

    Render public index

--- routes/app.js
+++ routes/app.js
@@ -1,26 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require ('../models/user');
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(error, document){
-        if(error){
-            return res.send('error');
-        }
-        res.render('node', {email: document.email});
-    });
-});
-
-router.post('/', function(req, res, next){
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Keil',
-        lastName: 'Carpenter',
-        password: 'password',
-        email: email
-    });
-    user.save(); // Store user object in db
-    res.redirect('/');
+        res.render('index');
 });
 
 module.exports = router;
