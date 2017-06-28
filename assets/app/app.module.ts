f9e97f72b82ebaea43a4663f4843057826ab17cf
Keil Carpenter <development@keilcarpenter.pro>
2017-06-28 18:37:33 +1200

    components and models folder

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from "./components/app/app.component";
 
 @NgModule({
     declarations: [
         AppComponent
     ],
-    imports: [BrowserModule],
+    imports: [BrowserModule,
+                FormsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
