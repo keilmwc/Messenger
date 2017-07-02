aa8e242f4585c14df5599054f30df629e3a162c5
Keil Carpenter <development@keilcarpenter.pro>
2017-07-02 19:36:56 +1200

    http get request - retrieve all messaged

--- routes/messages.js
+++ routes/messages.js
@@ -6,6 +6,22 @@ var router = express.Router();
 
 var Message = require('../models/message');
 
+router.get('/', function(req, res, next){
+    Message.find()
+        .exec(function(error, messages){
+           if(error){
+               return res.status(500).json({
+                   title: 'Oops! An error occurred!',
+                   error: error
+               });
+           }
+           res.status(200).json({
+               message: 'Messages successfully retrieved!',
+               object: messages
+           })
+        });
+});
+
 router.post('/', function(req, res, next){
     var message = new Message({
         content: req.body.content
@@ -17,7 +33,7 @@ router.post('/', function(req, res, next){
                 error: error
             });
         }
-        res.status(201).json({
+        res.status(200).json({
             message: 'Message Saved',
             object: result
         })
